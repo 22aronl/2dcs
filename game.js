79563099dfbb3ead361011156ab5e8517d9273d4
Aaron Lo <22aaronl@students.harker.org>
2020-10-06 23:19:27 -0700

    css

---
+++ game.js
@@ -0,0 +1,20 @@
+class Game {
+    contructor(io) {
+        this.io = io;
+    }
+
+    update() {
+
+    }
+
+    getUpdates() {
+
+    }
+}
+
+class Player {
+
+}
+
+
+module.exports.Game = Game;
--- server.js
+++ server.js
@@ -2,13 +2,17 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var socketIO = require('socket.io');
+var _game = require('./game');
+
+var Game = _game.Game;
 
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
+var game;
 
 app.set('port', 5000);
-app.use('/static', express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'));
 app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname, '/static/index.html'));
 });
@@ -19,6 +23,19 @@ if (port == null || port == "") {
 }
 
 
+
 server.listen(port, function() {
     console.log('Starting server on port 5000');
 });
+game = new Game(io);
+
+io.on('connection', function(socket) {
+    socket.on('new player', function() {
+
+    });
+});
+
+setInterval(() => {
+    game.update();
+    io.emit("update", game.getUpdates());
+}, 1000 / 30);
